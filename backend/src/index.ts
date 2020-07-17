6bddc4550fabe0b07fad41b2d9fc497ee4826159
Chibuzo Anyanka <chibuzoanyanka@Chibuzos-MBP.fritz.box>
2020-07-17 21:35:08 +1200

    update to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,9 @@ import { createConnection } from 'typeorm'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import { Request, Response } from 'express'
+import { GraphQLServer } from 'graphql-yoga'
+
 import { Routes } from './routes'
-import { User } from './entity/User'
 
 createConnection()
 	.then(async (connection) => {
@@ -35,27 +36,25 @@ createConnection()
 			)
 		})
 
+		//Graphql Connection Here
+		const typeDefs = `
+		type Query{
+			hello(name: String): String!
+		}
+		`
+		const resolvers = {
+			Query: {
+				hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+			},
+		}
+
+		const server = new GraphQLServer({ typeDefs, resolvers })
+		server.start(() => console.log('Server is runnings'))
 		// setup express app here
 		// ...
 
 		// start express server
 		app.listen(3000)
-
-		// insert new users for test
-		await connection.manager.save(
-			connection.manager.create(User, {
-				firstName: 'Timber',
-				lastName: 'Saw',
-				age: 27,
-			})
-		)
-		await connection.manager.save(
-			connection.manager.create(User, {
-				firstName: 'Phantom',
-				lastName: 'Assassin',
-				age: 24,
-			})
-		)
 
 		console.log(
 			'Express server has started on port 3000. Open http://localhost:3000/users to see results'
